0f802e3862a72dd1f99841a70653e74d54af4c80
ota <ota@bengo4.com>
2019-02-13 20:23:39 +0900

    JS少し調整

--- translation-contact.js
+++ translation-contact.js
@@ -2,12 +2,16 @@
  * 翻訳文書の問い合わせ先を書き換える
  * `<a href="" id="file-issue"></a>`のURLとリンクテキストを書き換えます。
  */
- addEventListener("DOMContentLoaded", function(){    
-  let viewurl = encodeURIComponent(window.location.href);
-  let sendurl = "https://docs.google.com/forms/d/e/1FAIpQLSdIpvogLx8kGIMewhQ6MzhG2pOCQZ50iIBViGg8pUrRJuslKg/viewform?entry.685195438=" + viewurl;
-  if (document.getElementById("file-issue") != null) {
-    let target = document.getElementById("file-issue");
-    target.setAttribute("href", sendurl);
-    target.textContent = "Google フォーム"
-  }
+const form_url_prefix = "https://docs.google.com/forms/d/e/1FAIpQLSdIpvogLx8kGIMewhQ6MzhG2pOCQZ50iIBViGg8pUrRJuslKg/viewform?entry.685195438=";
+const form_link_text = "翻訳に関するコメント (Google フォーム)";
+const target_id = "file-issue";
+
+document.addEventListener("DOMContentLoaded", () => {    
+    const viewurl = encodeURIComponent(window.location.href);
+    const sendurl = form_url_prefix + viewurl;
+    const target = document.getElementById(target_id);
+    if (target != null) {
+        target.setAttribute("href", sendurl);
+        target.textContent = form_link_text;
+    }
 });
